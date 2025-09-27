08eada1fb0860ea27277b18122f88590c9dbde22
Wai Yan <waiyanpaing183@gmail.com>
2025-09-27 17:44:07 +0700

    fixed basepath

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import { withBase } from "@/lib/basePath"; // adjust path if not using @ alias
+import { withBase } from "@/lib/basePath"; 
+import { withBase } from "../../lib/basePath";// adjust path if not using @ alias
 
 export default function BoxBasic() {
   return (
@@ -9,8 +10,8 @@ export default function BoxBasic() {
       <Box component="section" className="border border-gray-800 m-5 text-center">
         <h1 className="text-3xl text-violet-950">Stock Management v1.0</h1>
         <ul>
-          <li><a href={withBase("/product")}>Products</a></li>
-          <li><a href={withBase("/category")}>Category</a></li>
+          <li><a href="/stock/product">Products</a></li>
+          <li><a href="/stock/category">Category</a></li>
         </ul>
       </Box>
     </main>
